725bc36622ae22b6485df8dc77108adf09717926
Marcin Brzeziński <cintrzyk@gmail.com>
2017-12-09 17:26:19 +0100

    aggregateInHours

--- client/Dashboard.js
+++ client/Dashboard.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import moment from 'moment';
 
 class Dashboard extends Component {
-  state = { items: [] };
+  state = {
+    items: [],
+  };
   firebaseRef = firebase.database().ref('events');
 
   componentWillMount() {
@@ -13,6 +16,23 @@ class Dashboard extends Component {
 
   componentWillUnmount() {
     this.firebaseRef.off();
+  }
+
+  aggregateInHours() {
+    const res = {};
+    const getHour = date => moment.unix(date).format('H');
+    [...Array(24).keys()].forEach((hour) => {
+      const reducer = (accu, range) => {
+        accu[hour] = accu[hour] || 0;
+        if (getHour(range.startAt) <= hour && hour <= getHour(range.endAt) <= hour) {
+          accu[hour]++;
+        }
+        return accu;
+      };
+      res[hour] = this.state.items.reduce(reducer, {});
+    });
+
+    return res;
   }
 
   render() {
