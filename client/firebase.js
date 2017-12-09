ffa219092a23a435f29c2d86a0815c5fee0c5e77
Maciej Tatol <maciej.tatol@netguru.pl>
2017-12-09 15:28:54 +0100

    firebase config

---
+++ client/firebase.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCU3tMiE0b0Diin6Ot6Zsn8_0jkDsIrjRQ',
+  databaseURL: 'https://trader-f43d0.firebaseio.com/',
+};
+
+firebase.initializeApp(config);
