d26d8b3260ec6c7c618e979724e9c084528d12ca
Marek Buszman <mbusiq@gmail.com>
2017-12-09 15:27:13 +0100

    extract db connection to separate file

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import React from 'react';
-import * as firebase from 'firebase-admin';
 import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 import DashboardApp from '../client/Dashboard';
 import Layout from '../client/Layout';
 import botRoutes from './routes/bot';
 import slack from './slack';
-
-
-var admin = require("firebase-admin");
-
-const serviceAccount = require("../firebaseAccountKey.json");
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://trader-f43d0.firebaseio.com"
-});
-
-const db = firebase.database();
 
 const { SSR } = process.env;
 
