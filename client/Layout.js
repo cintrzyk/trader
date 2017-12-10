a1092e0684e210099384dde1af67e65ad55b33b3
Marcin Brzeziński <cintrzyk@gmail.com>
2017-12-10 08:43:21 +0100

    load font

--- client/Layout.js
+++ client/Layout.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Layout = props => (
   <html lang="en">
     <head>
       <title>{props.title}</title>
-      <link rel="stylesheet" type="text/css" href="http://localhost:3001/app.css" />
+      <link href="https://fonts.googleapis.com/css?family=Oswald:200,400,500" rel="stylesheet" />
+      <link rel="stylesheet" type="text/css" href="http://localhost:3001/styles-bundle.css" />
     </head>
     <body>
-      <div id="react-app" dangerouslySetInnerHTML={{ __html: props.children }} />
+      <div id="react-app" className="app" dangerouslySetInnerHTML={{ __html: props.children }} />
       <script src="http://localhost:3001/app.js" />
     </body>
   </html>
 );
+
+Layout.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string.isRequired,
+};
 
 export default Layout;
