7c6f6aac0d12a618c059ba2e130174a6346e2b2a
Marcin Brzeziński <cintrzyk@gmail.com>
2018-01-16 09:35:59 +0100

    waiting users list

--- client/Dashboard.js
+++ client/Dashboard.js
@@ -3,32 +3,37 @@ import firebase from 'firebase';
 import moment from 'moment';
 import StatsBadge from './components/StatsBadge';
 import Panel from './components/Panel';
+import WaitingUsers from './components/WaitingUsers';
 
 class Dashboard extends Component {
   state = {
     pending: 0,
     unassigned: 0,
     doneToday: [],
+    waitingUsers: [],
   };
 
   db = firebase.firestore();
+  limit = 200; // do not load too many documents
 
   componentDidMount() {
     this.listenToUnassigned();
     this.listenToPending();
     this.listenToAvgTimeToday();
+    this.listenToWaitingUsers();
   }
 
   componentWillUnmount() {
     this.unsubscribeUnassigned();
     this.unsubscribePending();
     this.unsubscribeAvgTimeToday();
+    this.unsubscribeWaitingUsers();
   }
 
   listenToUnassigned() {
     this.unsubscribeUnassigned = this.db.collection('slack_messages')
-      .where('cr_user_id', '>=', 2)
-      .limit(1000)
+      .where('cr_user_id', '==', null)
+      .limit(this.limit)
       .onSnapshot(snap => this.setState({ unassigned: snap.size }));
   }
 
@@ -36,7 +41,8 @@ class Dashboard extends Component {
     this.unsubscribePending = this.db.collection('slack_messages')
       .where('cr_start_at', '>=', new Date('1990')) // check if set
       .where('cr_end_at', '==', null)
-      .limit(1000)
+      .orderBy('cr_start_at', 'desc')
+      .limit(this.limit)
       .onSnapshot(snap => this.setState({ pending: snap.size }));
   }
 
@@ -44,7 +50,7 @@ class Dashboard extends Component {
     this.unsubscribeAvgTimeToday = this.db.collection('slack_messages')
       .where('cr_end_at', '>=', moment().startOf('day').toDate())
       .where('cr_end_at', '<=', moment().endOf('day').toDate())
-      .limit(1000)
+      .limit(this.limit)
       .onSnapshot((snap) => {
         if (!snap.size) {
           this.setState({ doneToday: [] });
@@ -53,6 +59,34 @@ class Dashboard extends Component {
         const data = [];
         snap.forEach(doc => data.push(doc.data()));
         this.setState({ doneToday: data });
+      });
+  }
+
+  listenToWaitingUsers() {
+    this.unsubscribeWaitingUsers = this.db.collection('slack_messages')
+      .where('cr_user_id', '==', null)
+      .orderBy('trade_available_at', 'desc')
+      .limit(10)
+      .onSnapshot((snap) => {
+        this.setState({ waitingUsers: [] });
+        if (!snap.size) {
+          return;
+        }
+        snap.forEach((doc) => {
+          const { gh_pr_id, trade_available_at } = doc.data();
+          this.db.collection('gh_prs').doc(gh_pr_id).get().then((prDoc) => {
+            const userId = prDoc.data().user_id;
+            this.db.collection('gh_users').doc(userId.toString()).get().then((userDoc) => {
+              this.setState(prevState => ({
+                ...prevState,
+                waitingUsers: prevState.waitingUsers.concat({
+                  ...userDoc.data(),
+                  trade_available_at,
+                }),
+              }));
+            });
+          });
+        });
       });
   }
 
@@ -74,7 +108,7 @@ class Dashboard extends Component {
           />
           <StatsBadge
             label="Ongoing"
-            icon="github"
+            icon="clock-o"
             value={this.state.pending}
           />
           <StatsBadge
@@ -88,7 +122,8 @@ class Dashboard extends Component {
             value={this.avgTimeInSecToday()}
           />
         </div>
-        <Panel title="Code Review over time">
+        <Panel title="Waiting for review">
+          <WaitingUsers data={this.state.waitingUsers} />
         </Panel>
       </div>
     );
---
+++ client/components/WaitingUsers.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FromNowDate from './FromNowDate';
+import '../styles/components/waiting-users.scss';
+
+const WaitingUser = props => (
+  <div className="waiting-user">
+    <div className="waiting-user__avatar">
+      <img src={props.avatar_url} alt={props.login} className="waiting-user__img" />
+    </div>
+    <div className="waiting-user__content">
+      <div className="waiting-user__name">
+        {props.login}
+      </div>
+      <div className="waiting-user__time">
+        <FromNowDate date={props.trade_available_at} />
+      </div>
+    </div>
+  </div>
+);
+
+const WaitingUsers = ({ data }) => {
+  const mappedUsers = data.map(userData => <WaitingUser key={userData.id} {...userData} />);
+
+  return (
+    <div className="waiting-users">
+      {mappedUsers}
+    </div>
+  );
+};
+
+WaitingUsers.defaultProps = {
+  icon: 'question',
+  label: 'todo',
+  value: '--',
+};
+
+WaitingUsers.propTypes = {
+  icon: PropTypes.string,
+  label: PropTypes.string,
+  value: PropTypes.node,
+};
+
+export default WaitingUsers;
