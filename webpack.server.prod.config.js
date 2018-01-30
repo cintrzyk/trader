9389c77a1792d8f00b456f2855956c05506ddd8d
Marcin Brzeziński <cintrzyk@gmail.com>
2018-01-30 23:27:28 +0100

    support async/await syntax

--- webpack.server.prod.config.js
+++ webpack.server.prod.config.js
@@ -4,7 +4,7 @@ const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge({
-  entry: path.resolve(__dirname, 'server.js'),
+  entry: ['babel-polyfill', path.resolve(__dirname, 'server.js')],
   target: 'node',
   output: {
     filename: 'server.bundle.js',
